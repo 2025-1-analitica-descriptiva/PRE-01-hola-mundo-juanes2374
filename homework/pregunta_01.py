bdaed51ef0ddd57960a6e3ce72ebf7d021d5a1a7
juanes2374 <jueposadama@unal.edu.co>
2025-04-12 11:33:38 -0500

    Terminado

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -8,13 +8,10 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 def pregunta_01():
     """
     Retorne el string "Hola mundo cruel!".
-
-    Rta/
-    Hola mundo cruel!
-
     """
     return "Hola mundo cruel!"
 
-
 if __name__ == "__main__":
     print(pregunta_01())
+
+
